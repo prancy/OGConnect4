1b681c6ca162b53e6a17a43a54997ddf948d79a2
nancy nguyen <prancynguyen@gmail.com>
2017-10-12 14:46:29 -0700

    finally added reset

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ var winner;
 var $cells = $('td');
 
 /*----- event listeners -----*/
+$('#play').on('click', function() {
+    init();
+    render();
+})
 $('.btns').on('click', function(evt) {
     var colIdx = parseInt(evt.target.id),
         rowIdx = board[colIdx].indexOf(0)
@@ -18,7 +22,7 @@ $('.btns').on('click', function(evt) {
     turn = turn * -1
     if (winner) return 
     winner = getWinner(colIdx, rowIdx)
-    console.log(`winner is ${winner}`)
+    // console.log(`winner is ${winner}`)
     render()
 })
 
@@ -40,11 +44,22 @@ function init() {
 
 function render () {
     var color = {
-         "-1": '#4ece90',
+         "-1": 'red',
          "0": 'white',
-         "1": '#52361b' 
+         "1": 'black' 
     };
-
+    // player's message
+    if (turn === 1) {
+        $('#message').html("Player 1's Turn")
+    } else {
+        $('#message').html("Player 2's Turn")
+    }
+    //winning message
+    if (winner === 1) {
+        $('#message').html("Player 1 Wins!")
+    } else if (winner === -1) {
+        $('#message').html("Player 2 Wins!")
+    }
     $cells.each(function() {
         var $cell = $(this); 
         var colIdx = $cell.attr('data-col');
@@ -58,6 +73,9 @@ function getWinner(colIdx, rowIdx) {
     if (winner) return;
     for (var colIdx = 0; colIdx < 6; colIdx++) {
         for (var rowIdx = 0; rowIdx < 5; rowIdx++ ) {
+            if (checkRight) {
+
+        } 
             winner = checkRight(colIdx, rowIdx) || checkUp(colIdx, rowIdx) || checkDiagUp(colIdx, rowIdx) || checkDiagDown(colIdx, rowIdx)
             if (winner) return winner
         }
@@ -80,17 +98,17 @@ function checkUp(colIdx, rowIdx) {
 }
 
 // Going RIGHT and UP
-function checkDiagUp (colIdx, rowIdx) {
-    if (rowIdx < 3 || colIdx > 3 ) return null;    
-    var sum = Math.abs(board[rowIdx][colIdx] + board[rowIdx - 1][colIdx + 1] + board[rowIdx - 2][colIdx + 2] + board[rowIdx - 3][colIdx + 3]);
-    return (sum === 4) ? board[rowIdx][colIdx] : null;
+function checkDiagDown (colIdx, rowIdx) {
+    if (colIdx > 3 || rowIdx < 3) return null;    
+    var sum = Math.abs(board[colIdx][rowIdx] + board[colIdx + 1][rowIdx - 1] + board[colIdx + 2][rowIdx - 2] + board[colIdx + 3][rowIdx - 3]);
+    return (sum === 4) ? board[colIdx][rowIdx] : null;
 }
 
 // Going RIGHT and DOWN
-function checkDiagDown (colIdx, rowIdx) {
-    if (colIdx < 4 || rowIdx > 2) return null;
-    var sum = Math.abs(board[rowIdx][colIdx] + board[rowIdx + 1][colIdx + 1] + board[rowIdx+2][colIdx + 2] + board[rowIdx + 3][colIdx + 3]);
-    return (sum === 4) ? board[rowIdx][colIdx] : null;
+function checkDiagUp(colIdx, rowIdx) {
+    if (colIdx > 3 || rowIdx > 2) return null;   
+    var sum = Math.abs(board[colIdx][rowIdx] + board[colIdx + 1][rowIdx + 1] + board[colIdx + 2][rowIdx + 2] + board[colIdx + 3][rowIdx + 3]);
+    return (sum === 4) ? board[colIdx][rowIdx] : null;
 }
 
 init();
